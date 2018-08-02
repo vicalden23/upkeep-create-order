b3c6617ec9453bef0dcb7954b782351c8d16bd47
vic.alden23 <vic.alden23@gmail.com>
2018-08-01 19:24:41 -0700

    set up display for work orders

--- src/components/workOrders.js
+++ src/components/workOrders.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import axios from 'axios';
+import '../App.css'
 
 class WorkOrders extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      orders: []
+    }
+  }
+
+  componentDidMount() {
+    axios.post('https://api.onupkeep.com/api/v2/auth/', {
+      email:'test+0726@onupkeep.com',
+      password: 'testing'
+    })
+    .then((auth) => {
+      let options = {
+        method: 'GET',
+        headers: { 'Session-Token': auth.data.result.sessionToken },
+        url: 'https://api.onupkeep.com/api/v2/work-orders'
+      }
+      
+      axios(options)
+      .then((result) => {
+        this.setState({
+          orders: result.data.results
+        })
+      })
+    })
+  }
 
   render() {
     return (
-      <div>
-        <Button>
-          <Link to='/'>
+      <div className='order-container'>
+        <div className='title'>
+          <h4>Work Orders</h4>
+          <Link to='/' className='link'>
             Create Work Order
           </Link>
-        </Button>
-        <h4> All Orders </h4>
+        </div>
+        <div className='order-list'>
+          {
+            this.state.orders.length ? this.state.orders.map((order, i) => {
+              return (
+                <div className='order-card' key={i}>
+                  <p className='status'>{order.status}</p>
+                  <h3>{order.title}</h3>
+                  <p>{order.description}</p>
+                </div>
+              )
+            })
+
+            : ''
+          }
+        </div>
       </div>
     );
   }
